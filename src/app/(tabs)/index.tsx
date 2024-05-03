a97d05c0ffe96315f07f224d55b0e2b51180cc68
the-me-0 <119881173+the-me-0@users.noreply.github.com>
2024-05-03 08:58:10 -0400

    5. Navigation
    Added a detail page for products, a deep navigation system, and some style for the navbar & headers. Link and nav components overall.

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,19 +1,5 @@
-import {FlatList, View} from 'react-native';
-import products from '@assets/data/products';
-import { ProductListItem } from '@/components/ProductListItem';
+import { Redirect } from 'expo-router';
 
-export default function TabOneScreen() {
-  return (
-    <View>
-      {/*<ProductListItem product={products[0]} />*/}
-      {/*<ProductListItem product={products[1]} />*/}
-      <FlatList
-        data={products}
-        renderItem={({item}) => <ProductListItem product={item} />}
-        numColumns={2}
-        contentContainerStyle={{ gap: 10, padding: 10 }}
-        columnWrapperStyle={{ gap: 10 }}
-      />
-    </View>
-  );
+export default function TabIndex() {
+  return <Redirect href={'/menu/'} />;
 }
---
+++ src/app/(tabs)/menu/[id].tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { useLocalSearchParams } from 'expo-router';
+
+const ProductDetailsScreen = () => {
+  const { id } = useLocalSearchParams();
+
+  return (
+    <View>
+      <Text>ProductDetailsScreen: {id}</Text>
+    </View>
+  );
+}
+
+export default ProductDetailsScreen;
